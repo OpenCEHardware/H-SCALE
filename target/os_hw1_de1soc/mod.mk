92b297ab51c47f826e404d470c1dc4b4c08e10d5
Alejandro Soto <alejandro@34project.org>
2024-10-15 23:27:30 -0600

    target/os_hw1: build software ROM image along with bitstream

--- target/os_hw1_de1soc/mod.mk
+++ target/os_hw1_de1soc/mod.mk
@@ -1,4 +1,9 @@
 define core
+  $(this)/deps  := hsv_host_sw
+  $(this)/hooks := obj
+
+  $(this)/obj_deps := hsv_host_sw.hex
+
   $(this)/rtl_top   := cpu_inst
   $(this)/rtl_files := cpu_inst.v
 
--- target/os_hw1_de1soc/rom.tcl
+++ target/os_hw1_de1soc/rom.tcl
@@ -1,1 +1,1 @@
-set_global_assignment -name HEX_FILE cpu_memoria.hex
+set_global_assignment -name HEX_FILE hsv_host_sw.hex
