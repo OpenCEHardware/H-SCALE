e87eee5dc1a01845d64c4c8fc5d66a8b25be68db
Jose Alejandro Chavarria Madriz <joalchama@gmail.com>
2024-10-16 00:59:13 -0600

    Add sistema heterogeneo

--- target/os_hw1_de1soc/mod.mk
+++ target/os_hw1_de1soc/mod.mk
@@ -2,8 +2,6 @@ define core
   $(this)/deps  := hsv_host_sw
   $(this)/hooks := obj
 
-  $(this)/obj_deps := hsv_host_sw.hex
-
   $(this)/rtl_top   := cpu_inst
   $(this)/rtl_files := cpu_inst.v
 
@@ -12,6 +10,6 @@ define core
 
   $(this)/sdc_files     := timing.sdc
   $(this)/qsf_files     := rom.tcl pins.tcl
-  $(this)/qsys_deps     := hsv_core_top_flat
+  $(this)/qsys_deps     := hsv_core_top_flat hs_npu_top_flat
   $(this)/qsys_platform := cpu.qsys
 endef
